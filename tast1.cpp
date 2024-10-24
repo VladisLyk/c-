c4b7e552ee2b89017e8fdfd024d7f24cb7ea0377
userit <userit@oq.oq>
2024-10-24 22:04:04 +0300

    lab2-3

---
+++ tast1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Якщо змінна не визначена, показує номер комірки в якій записану цю змінну
+main() {
+
+	int i;
+	int j = 0;
+	
+	std::cout << "Not defined variable: " << i << "\n";
+	std::cout << "Defined varialble: " << j;
+}
